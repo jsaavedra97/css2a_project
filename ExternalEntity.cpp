dbd408dbd11d9936df7b34f391eabe8270e82a33
sampsonliao <sliao@csumb.edu>
2017-05-11 20:59:35 -0700

    entity update

--- ExternalEntity.cpp
+++ ExternalEntity.cpp
@@ -1,11 +1,27 @@
 #include "ExternalEntity.h"
+#include <iostream>
+using namespace std;
 
 ExternalEntity::ExternalEntity()
 {
-    //ctor
+    shape.setSize(sf::Vector2f(20.0f, 100.0f));
+    shape.setFillColor(sf::Color::White);
+    shape.setOrigin(10.0f, 0.0f);
+    boundingBox = shape.getGlobalBounds();
+}
+ExternalEntity::ExternalEntity(const sf::Vector2f& dim, const string&  file_name)
+{
+    texture.loadFromFile(file_name);
+    cout << file_name << endl;
+
+    shape.setSize(dim);
+    shape.setFillColor(sf::Color::White);
+    shape.setTexture(&texture);
+    shape.setOrigin(dim.x/2, 0.0f);
+    boundingBox = shape.getGlobalBounds();
 }
 void ExternalEntity::setPosition(sf::Vector2f xy)
 {
-
+    shape.setPosition(xy);
 }
 
--- ExternalEntity.h
+++ ExternalEntity.h
@@ -6,17 +6,18 @@
 #include <cassert>
 #include <iostream>
 
+using namespace std;
 //sdfsdfs
 class ExternalEntity
 {
     public:
         ExternalEntity();
+        ExternalEntity(const sf::Vector2f& dim,const string&  file_name);
         virtual ~ExternalEntity(){}
         sf::RectangleShape getShape()const{return shape;}
         sf::FloatRect getBoundingBox()const{return boundingBox;}
         void setPosition(sf::Vector2f xy);
         virtual bool update()=0;
-
 
         sf::RectangleShape shape;
         sf::Texture texture;
