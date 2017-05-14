671a7b6ac247b859130d807d5bd46525e2368dea
sampsonliao <sliao@csumb.edu>
2017-05-14 10:23:00 -0700

    moved enviroment parameter from ship to player alone

--- Ship.cpp
+++ Ship.cpp
@@ -11,11 +11,10 @@ Ship::Ship():ExternalEntity()
     sprite.setOrigin(100.0f, 100.0f);
     projectiles = new Projectile;
 }
-Ship::Ship(string img_path, int health, const Projectile& p, const sf::Vector2f& start_pos, const Environment& e):ExternalEntity(img_path)
+Ship::Ship(string img_path, int health, const Projectile& p, const sf::Vector2f& start_pos):ExternalEntity(img_path)
 {
     assert(img_path != "");
     assert(health > 1);
-    assert(start_pos.x > 0 && start_pos.y > 0 && start_pos.x < e.getWidth() && start_pos.y < e.getHeight());
     this->health = health;
     sprite.setOrigin(sprite.getLocalBounds().width/2, 0);
 //    sprite.setPosition(sf::Vector2f(start_pos.x-sprite.getGlobalBounds().width/2, start_pos.y));
